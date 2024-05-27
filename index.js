630121446e6db73f5910ca3a58da5bfaf76b4757
sachinm121 <112053922+sachinm121@users.noreply.github.com>
2024-05-27 22:15:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://campusvoice.vercel.app",
     credentials: true
 }))
 
